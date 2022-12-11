64475d421308dfe1af9823398e46cd594e7983f7
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2022-12-11 12:14:38 -0300

    refactor: return an object of separate numbers

--- src/benford/separate-numbers/separate-number-by-first-digit.ts
+++ src/benford/separate-numbers/separate-number-by-first-digit.ts
@@ -5,9 +5,13 @@ export const separateNumberByFirstDigit = (array: number[]) => {
     const firstDigit = Number(number.toString().charAt(0));
     arrayAux[firstDigit].push(number);
   });
-  arrayAux.splice(0, 1);
 
-  return `ArrayAux length: ${arrayAux} <br/> ${arrayAux.map(
-    (item, index) => `<br/>first digit:${++index}  quantidade:${item.length}`
-  )}`;
+  return arrayAux.reduce(
+    (prev, value, index) => ({
+      ...prev,
+      [index]: { numbers: value, total: value.length },
+      total: array.length,
+    }),
+    {}
+  );
 };

95ddf5979d3648614f38ad199d29495f5d1e5c11
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2022-12-24 03:31:33 -0300

    feat: allow 40mb files

--- example/services/uploadFile.ts
+++ example/services/uploadFile.ts
@@ -1,7 +1,7 @@
 import multer from 'multer';
 import util from 'util';
 import path from 'path';
-const maxSize = 2 * 1024 * 1024;
+const maxSize = 40 * 1024 * 1024;
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {

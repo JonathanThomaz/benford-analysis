1ea8aeac5b57d16f647f40a3841dfa9b39226178
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2022-12-24 03:30:36 -0300

    feat: create function to return deviation percent by benford expected

--- src/benford/law/index.ts
+++ src/benford/law/index.ts
@@ -1,1 +1,15 @@
 export const expectPercentByFirstDigit = (d: number) => Math.log10(1 + 1 / d);
+
+export const getDeviationByFirstDigit = (d: number, percent: number) => {
+  const expectPercent = expectPercentByFirstDigit(d);
+  const deviation =
+    percent > expectPercent
+      ? (percent - expectPercent) / percent
+      : (expectPercent - percent) / percent;
+  return {
+    digit: d,
+    expectPercent,
+    percent: percent,
+    deviation,
+  };
+};

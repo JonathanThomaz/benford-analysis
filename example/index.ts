8a41e6a00c51a4b7b72b5441ef19eb2547d87c46
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2022-12-24 03:32:03 -0300

    feat: create new routes

--- example/index.ts
+++ example/index.ts
@@ -1,16 +1,38 @@
-import express, { NextFunction } from 'express';
+import express from 'express';
 import { readCsv, separateNumberByFirstDigit } from '../dist';
 import uploadFile from './services/uploadFile';
+import bodyParser from 'body-parser';
+import {
+  getBenfordFirstDigitExpected,
+  getDeviationByFirstDigit,
+} from './../src/benford/law/index';
 
 const app = express();
 
 const port = 3333;
-const array = [
-  10230, 1293, 60909, 70909, 80045, 945904, 10203, 234030, 2394839, 2384938,
-  29349, 340940, 49549, 506090,
-];
-app.get('/', (req, res) => {
-  res.send(separateNumberByFirstDigit(array));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+
+app.get('/benford-law', (req, res) => {
+  res.status(200).send(getBenfordFirstDigitExpected());
+});
+
+app.post('/separate', (req, res, next) => {
+  const { array } = req.body;
+  if (array) {
+    const arrayNumber = array.filter((item: number | string) => Number(item));
+    res.status(200).send(separateNumberByFirstDigit(arrayNumber));
+  }
+  res.status(400).send('Please send an array');
+});
+app.post('/benford-law', (req, res, next) => {
+  const { d1, d2, d3, d4, d5, d6, d7, d8, d9 } = req.body;
+  const array = [d1, d2, d3, d4, d5, d6, d7, d8, d9];
+  res.send(
+    array.map((item, index) =>
+      getDeviationByFirstDigit(index + 1, item.percent)
+    )
+  );
 });
 
 app.post('/read-csv', async (req, res, next) => {
